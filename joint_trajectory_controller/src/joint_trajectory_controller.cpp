bf273f678ac484f1cf70f2ee64a9937d3592d03e
Lovro <lovro@standardbots.com>
2026-05-04 17:36:54 +0200

    Result timeout change.

--- joint_trajectory_controller/src/joint_trajectory_controller.cpp
+++ joint_trajectory_controller/src/joint_trajectory_controller.cpp
@@ -903,13 +903,22 @@ controller_interface::CallbackReturn JointTrajectoryController::on_configure(
   action_monitor_period_ = rclcpp::Duration::from_seconds(1.0 / params_.action_monitor_rate);
 
   using namespace std::placeholders;
+
+  // Reduce result_timeout from the 900 s (15 min) default to 3 s.
+  // The default causes unbounded GoalStatusArray growth in CycloneDDS
+  // (see rmw_cyclonedds#388). 3 s is safe because execute_server registers
+  // its result future before the goal completes.
+  rcl_action_server_options_t action_server_options = rcl_action_server_get_default_options();
+  action_server_options.result_timeout.nanoseconds = RCUTILS_S_TO_NS(3);
+
   action_server_ = rclcpp_action::create_server<FollowJTrajAction>(
     get_node()->get_node_base_interface(), get_node()->get_node_clock_interface(),
     get_node()->get_node_logging_interface(), get_node()->get_node_waitables_interface(),
     std::string(get_node()->get_name()) + "/follow_joint_trajectory",
     std::bind(&JointTrajectoryController::goal_received_callback, this, _1, _2),
     std::bind(&JointTrajectoryController::goal_cancelled_callback, this, _1),
-    std::bind(&JointTrajectoryController::goal_accepted_callback, this, _1));
+    std::bind(&JointTrajectoryController::goal_accepted_callback, this, _1),
+    action_server_options);
 
   resize_joint_trajectory_point(state_current_, dof_);
   resize_joint_trajectory_point_command(command_current_, dof_);
